af38ba026c9c1f1eea0f15972d3a8e5de859944c
Akshay Jagtap <akshayajk111@gmail.com>
2022-04-26 23:14:59 +0530

    sec

---
+++ threeOperators/Arithmatic.js
@@ -0,0 +1,25 @@
+ let x = 10;
+ let y = 3;
+
+//  console.log(x + y);
+//  console.log(x - y);
+//  console.log(x * y);
+//  console.log(x / y);
+//  console.log(x % y);
+//  console.log(x ** y); //exponetion   ie. x is power of y
+
+ //increment (++)
+ console.log(++x);  //11
+
+// or
+console.log(x++);   //10
+onsole.log(x);   //11
+
+//Decrement (--)
+onsole.log(--y);    //2
+  
+
+
+
+
+ 
