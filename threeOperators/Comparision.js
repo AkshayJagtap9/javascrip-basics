104ea3c138fcc30d1108bd1935e70895e05be7e0
Akshay Jagtap <akshayajk111@gmail.com>
2022-04-26 23:25:58 +0530

    modified folder names with digits

---
+++ threeOperators/Comparision.js
@@ -0,0 +1,9 @@
+let x = 1;
+console.log(x > 0);     //true
+console.log(x >= 1);     //true
+
+console.log(x < 1);     //false
+console.log(x <= 1);     //true
+
+console.log(x == 1);     //true
+console.log(x != 1);     //true
