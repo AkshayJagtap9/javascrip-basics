96125d8d717dc83830b93b4b6f7c9266c990f759
Akshay Jagtap <akshayajk111@gmail.com>
2022-04-30 17:00:44 +0530

    shortcut of ternary operator

---
+++ 4ControlFlow/Ex2.js
@@ -0,0 +1,11 @@
+// if width is greter than height then return true else false 
+
+console.log(img(21, 16));   // 
+
+function img(width,height){
+    //return (width > height) ? true : false
+                //OR
+    return (width > height);
+}
+
+//true
