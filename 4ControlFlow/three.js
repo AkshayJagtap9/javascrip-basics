acd540be17a0efab8393afbb3c5a2cd29707bfa1
Akshay Jagtap <akshayajk111@gmail.com>
2022-04-30 10:57:25 +0530

    do while

--- 4ControlFlow/three.js
+++ 4ControlFlow/three.js
@@ -6,8 +6,15 @@ for(let i = 0; i<=5; i++){
 // 3
 // 5
 
-let i = 0;
-while(i<=5){
+// let i = 0;
+// while(i<=5){
+//     if(i % 2 !==0) console.log(i);
+//     i++;
+// }
+
+// do-while loop
+let i = 0; 
+do {
     if(i % 2 !==0) console.log(i);
     i++;
-}
+}while (i <= 5);
