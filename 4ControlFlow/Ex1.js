bc2f8966ad8b0263e871f4a9e120b32738b94521
Akshay Jagtap <akshayajk111@gmail.com>
2022-04-30 16:52:02 +0530

    Ternary Operator

---
+++ 4ControlFlow/Ex1.js
@@ -0,0 +1,31 @@
+// Write a function that takes two numbers and return
+// the maximum of the two
+
+function fun(num1 ,num2){
+   
+    var num1, num1
+
+    if(num1 > num2)
+    {
+        return num1;
+    }
+    else return num2;
+
+}
+
+console.log('Maximum of two numbers is: ' + fun(3,5));
+console.log('Maximum of two numbers is: ' + fun(9,7));
+
+
+                //OR
+
+let number = max(3,7);
+console.log(number);
+
+function max(a,b){
+    // if(a>b) return a;
+    // return b;
+            //or
+    return (a > b) ? a: b;         
+
+}
