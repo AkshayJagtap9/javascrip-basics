66bbb16053167f125f2968094665939b6d27ae06
Akshay Jagtap <akshayajk111@gmail.com>
2022-05-24 22:10:16 +0530

    Rest Operator

--- 7Functions/4RestOperator.js
+++ 7Functions/4RestOperator.js
@@ -3,3 +3,43 @@ function sum(...args){
 }
 
 console.log(sum(1,2,3,4,5,10)); //[ 1, 2, 3, 4, 5, 10 ]
+
+function sum(...args){
+    //return args.reduce((a,b) => a + b);       //25
+    console.log(args);
+}
+
+console.log(sum(1,2,3,4,5,10));     //[ 1, 2, 3, 4, 5, 10 ]             // output is real array
+
+
+                //OR
+/*
+function sum(args){
+    console.log(args);
+}         
+console.log(sum(1,2,3,4,5,10));     //1
+
+*/
+
+
+/*
+function sum(discount, ...prices){
+    const total =  prices.reduce((a,b) => a + b);
+    return total * (1 - discount);      //45
+}
+console.log(sum(0.1, 20, 30));
+
+*/
+
+/*
+
+function sum(discount, ...prices, someValue){
+    const total =  prices.reduce((a,b) => a + b);
+    return total * (1 - discount);      //45
+}
+console.log(sum(0.1, 20, 30, 1));
+
+op: Error : Rest parameter must be last formal parameter.
+*/
+
+
